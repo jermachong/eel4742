b873291ff5411e4b0694f19fd7a507d38f98ea3c
Jeremy Achong <jeremyachong@gmail.com>
2026-02-03 23:27:25 -0500

    using two push buttons

--- lab02/main.c
+++ lab02/main.c
@@ -1,7 +1,7 @@
 /*
 * EEL 4742
 * Jeremy Achong
-* Turning on the red LED while button S1 is pushed 
+* Lab 2 - Experimentation with using the board's buttons
 */
 
 #include <driverlib.h>
@@ -9,7 +9,41 @@
 #define redLED BIT0 // Red LED at P1.0
 #define greenLED BIT7 // Green LED at P9.7
 #define BUT1 BIT1 // Button S1 at P1.1
+#define BUT2 BIT2 // Button S2 at P1.2
 
+// 2.1: Turning on the red LED while button BUT1 is pushed 
+// int main(void) {
+
+//     volatile uint32_t i;
+
+//     WDTCTL = WDTPW | WDTHOLD; // Stop the Watchdog timer
+//     PM5CTL0 &= ~LOCKLPM5;     // Disable GPIO power-on default high-impedance
+
+//     // Configure and initialize LEDs
+//     P1DIR |= redLED; // Direct pin as output
+//     P9DIR |= greenLED; // Direct pin as output
+//     P1OUT &= ~redLED; // Turn LED Off
+//     P9OUT &= ~greenLED; // Turn LED Off
+
+//     // Configure buttons
+//     P1DIR &= ~BUT1; // Direct pin as input
+//     P1REN |= BIT1;        // Enable built-in resistor
+//     P1OUT |= BIT1;    // Set resistor as pull-up
+
+//     // Polling the button in an infinite loop
+//     for (;;) {
+//       // Rewrite the pseudocode below into C code
+//         if ((P1IN * BUT1)==0) { // button is pushed
+//             P1OUT ^= redLED;    
+//         }
+//         else {
+//             P1OUT &= ~redLED; // Turn LED Off
+//         }
+//     }
+// }
+
+
+// 2.2: Using Two Push Buttons
 int main(void) {
 
     volatile uint32_t i;
