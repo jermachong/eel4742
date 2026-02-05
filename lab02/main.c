a22a27d2716b56e0dfb5b094d8aff7b5d8a78774
Jeremy Achong <jeremyachong@gmail.com>
2026-02-05 14:34:29 -0500

    prototype for 2.2 and 2.3

--- lab02/main.c
+++ lab02/main.c
@@ -44,6 +44,48 @@
 
 
 // 2.2: Using Two Push Buttons
+// int main(void) {
+
+//     volatile uint32_t i;
+
+//     WDTCTL = WDTPW | WDTHOLD; // Stop the Watchdog timer
+//     PM5CTL0 &= ~LOCKLPM5;     // Disable GPIO power-on default high-impedance
+
+//     // Configure and initialize LEDs
+//     P1DIR |= redLED; // Direct pin as output
+//     P9DIR |= greenLED; // Direct pin as output
+//     P1OUT &= ~redLED; // Turn LED Off
+//     P9OUT &= ~greenLED; // Turn LED Off
+
+//     // Configure buttons
+//     P1DIR &= ~BUT1; // Direct pin as input
+//     P1REN |= BIT1;        // Enable built-in resistor
+//     P1OUT |= BIT1;    // Set resistor as pull-up
+//     P1DIR &= ~BUT2; // Direct pin as input
+//     P1REN |= BIT2;        // Enable built-in resistor
+//     P1OUT |= BIT2;    // Set resistor as pull-up
+
+//     // Polling the button in an infinite loop
+//     for (;;) {
+
+//         if ((P1IN * BUT1)==0) { // button is pushed
+//             P1OUT ^= redLED;    
+//         }
+//         else {
+//             P1OUT &= ~redLED; // Turn LED Off
+//         }
+//         if ((P1IN * BUT2)==0) { // button is pushed
+//             P1OUT ^= greenLED;        
+//         }
+//         else {
+//             P1OUT &= ~greenLED; // Turn LED Off
+//         }
+//     }
+// }
+
+// 2.3: Electric Load Generator (v1)
+// first come first serve, don't run at the same time
+// LED is on as long as button is pressed
 int main(void) {
 
     volatile uint32_t i;
@@ -61,15 +103,21 @@ int main(void) {
     P1DIR &= ~BUT1; // Direct pin as input
     P1REN |= BIT1;        // Enable built-in resistor
     P1OUT |= BIT1;    // Set resistor as pull-up
+    P1DIR &= ~BUT2; // Direct pin as input
+    P1REN |= BIT2;        // Enable built-in resistor
+    P1OUT |= BIT2;    // Set resistor as pull-up
 
     // Polling the button in an infinite loop
     for (;;) {
-      // Rewrite the pseudocode below into C code
         if ((P1IN * BUT1)==0) { // button is pushed
-            P1OUT ^= redLED;    
+            P1OUT |= redLED;    
+        }
+        else if ((P1IN * BUT2)==0) { // button is pushed
+            P9OUT |= greenLED;        
         }
         else {
             P1OUT &= ~redLED; // Turn LED Off
+            P9OUT &= ~greenLED; // Turn LED Off
         }
     }
 }
