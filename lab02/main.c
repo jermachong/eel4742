e205f703c427254ad1da7719cbee8002ea690845
Jeremy Achong <jeremyachong@gmail.com>
2026-02-12 18:22:08 -0500

    working final version that was checked off in lab

--- lab02/main.c
+++ lab02/main.c
@@ -46,18 +46,18 @@
 // first come first serve, don't run at the same time
 // LED is on as long as button is pressed
 // int main(void) {
-
+//
 //   volatile uint32_t i;
-
+//
 //   WDTCTL = WDTPW | WDTHOLD; // Stop the Watchdog timer
 //   PM5CTL0 &= ~LOCKLPM5;     // Disable GPIO power-on default high-impedance
-
+//
 //   // Configure and initialize LEDs
 //   P1DIR |= redLED;    // Direct pin as output
 //   P9DIR |= greenLED;  // Direct pin as output
 //   P1OUT &= ~redLED;   // Turn LED Off
 //   P9OUT &= ~greenLED; // Turn LED Off
-
+//
 //   // Configure buttons
 //   P1DIR &= ~BUT1; // Direct pin as input
 //   P1REN |= BIT1;  // Enable built-in resistor
@@ -65,7 +65,7 @@
 //   P1DIR &= ~BUT2; // Direct pin as input
 //   P1REN |= BIT2;  // Enable built-in resistor
 //   P1OUT |= BIT2;  // Set resistor as pull-up
-
+//
 //   // Polling the button in an infinite loop
 //   for (;;) {
 //     // If Button 1 is pressed AND Green LED is NOT currently on
@@ -74,7 +74,7 @@
 //     } else {
 //       P1OUT &= ~redLED;
 //     }
-
+//
 //     // If Button 2 is pressed AND Red LED is NOT currently on
 //     if (!(P1IN & BUT2) && !(P1OUT & redLED)) {
 //       P9OUT |= greenLED;
@@ -85,71 +85,67 @@
 // }
 
 // 2.4: Electric Load Generator (v2)
-// if button is pressed and another button is already running, turn off both
-// // LEDS until both buttons are released
-int main(void) {
+//// if button is pressed & other button already running, turn off both LEDS until both buttons are released
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD; // Stop the Watchdog timer
+    PM5CTL0 &= ~LOCKLPM5;     // Disable GPIO power-on default high-impedance
+    // Configure and initialize LEDs
+    P1DIR |= redLED;    // Direct pin as output
+    P9DIR |= greenLED;  // Direct pin as output
+    P1OUT &= ~redLED;   // Turn LED Off
+    P9OUT &= ~greenLED; // Turn LED Off
+    // Configure buttons
+    P1DIR &= ~BUT1; // Direct pin as input
+    P1REN |= BIT1;  // Enable built-in resistor
+    P1OUT |= BIT1;  // Set resistor as pull-up
+    P1DIR &= ~BUT2; // Direct pin as input
+    P1REN |= BIT2;  // Enable built-in resistor
+    P1OUT |= BIT2;  // Set resistor as pull-up
+    int b1, b2 = 0;
+    //   Polling the button in an infinite loop
+    for (;;)
+    {
+        // read button states
+        b1 = !(P1IN & BUT1); // red button pushed
+        b2 = !(P1IN & BUT2); // green button pushed
 
-  volatile uint32_t i;
-
-  WDTCTL = WDTPW | WDTHOLD; // Stop the Watchdog timer
-  PM5CTL0 &= ~LOCKLPM5;     // Disable GPIO power-on default high-impedance
-
-  // Configure and initialize LEDs
-  P1DIR |= redLED;    // Direct pin as output
-  P9DIR |= greenLED;  // Direct pin as output
-  P1OUT &= ~redLED;   // Turn LED Off
-  P9OUT &= ~greenLED; // Turn LED Off
-
-  // Configure buttons
-  P1DIR &= ~BUT1; // Direct pin as input
-  P1REN |= BIT1;  // Enable built-in resistor
-  P1OUT |= BIT1;  // Set resistor as pull-up
-  P1DIR &= ~BUT2; // Direct pin as input
-  P1REN |= BIT2;  // Enable built-in resistor
-  P1OUT |= BIT2;  // Set resistor as pull-up
-
-//   Polling the button in an infinite loop 
-  for (;;) {
-    // read button states
-    int b1 = !(P1IN & BUT1);
-    int b2 = !(P1IN & BUT2);
-    
-    if (b1 && !(P9OUT & greenLED)) { // red button is pushed and greenLED is not on
-        P1OUT |= redLED;
-    } else {
-        P1OUT &= ~redLED;
-    }
-    
-    if (b2 && !(P1OUT & redLED)) { // green button is pushed and redLED is not on
-        P9OUT |= greenLED;
-    } else {
-        P9OUT &= ~greenLED;
-    }
-
-    if(b1 && b2) { // red and green buttons are pushed, turn off both LEDs
-        if(P1OUT & redLED)
+        if (b1 && !(P9OUT & greenLED)) // red button is pushed and greenLED is not on
+            P1OUT ^= redLED;
+        else
             P1OUT &= ~redLED;
-        if(P9OUT & greenLED)
-            P9OUT &= ~greenLED;
-    }
-  }
-}
 
+        if (b2 && !(P1OUT & redLED)) // green button is pushed and redLED is not on
+            P9OUT |= greenLED;
+        else
+            P9OUT &= ~greenLED;
+
+        if (b1 && b2) // lockout
+        { // red and green buttons are pushed, turn off both LEDs
+            do
+            {
+                P1OUT &= ~redLED;
+                P9OUT &= ~greenLED;
+            }while(!(P1IN & BUT1)| !(P1IN & BUT2));
+
+        }
+    }
+}
 
 // 2.2: Using Two Push Buttons
 // int main(void) {
-//
+
 //     volatile uint32_t i;
-//
+
 //     WDTCTL = WDTPW | WDTHOLD; // Stop the Watchdog timer
 //     PM5CTL0 &= ~LOCKLPM5;     // Disable GPIO power-on default high-impedance
-//
+
 //     // Configure and initialize LEDs
 //     P1DIR |= redLED; // Direct pin as output
 //     P9DIR |= greenLED; // Direct pin as output
 //     P1OUT &= ~redLED; // Turn LED Off
 //     P9OUT &= ~greenLED; // Turn LED Off
-//
+
 //     // Configure buttons
 //     P1DIR &= ~BUT1; // Direct pin as input
 //     P1REN |= BIT1;        // Enable built-in resistor
@@ -157,10 +153,10 @@ int main(void) {
 //     P1DIR &= ~BUT2; // Direct pin as input
 //     P1REN |= BIT2;        // Enable built-in resistor
 //     P1OUT |= BIT2;    // Set resistor as pull-up
-//
+
 //     // Polling the button in an infinite loop
 //     for (;;) {
-//
+
 //         if ((P1IN & BUT1)==0) { // button is pushed
 //             P1OUT ^= redLED;
 //         }
