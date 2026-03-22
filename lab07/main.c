9135dc0ac1175a90d32ead895110a384b0a9ce79
Jeremy Achong <jeremyachong@gmail.com>
2026-03-21 20:41:13 -0400

    almost done with last part

--- lab07/main.c
+++ lab07/main.c
@@ -14,6 +14,8 @@
 #define TXFLAG UCTXIFG     // Transmit flag
 #define TXBUFFER UCA1TXBUF // Transmit buffer
 #define RXBUFFER UCA1RXBUF // Receive buffer
+#define BUT1 BIT1          // Button S1 at P1.1
+#define BUT2 BIT2          // Button S2 at P1.2
 
 void Initialize_I2C();
 void Initialize_UART();
@@ -50,6 +52,36 @@ void uart_write_string(char *str);
 // }
 
 // Part 7.2 Reading Measurments from the Light Sensor
+// int main() {
+//   WDTCTL = WDTPW | WDTHOLD; // stop watchdog timer
+//   PM5CTL0 &= ~LOCKLPM5;
+
+//   Initialize_UART();
+//   Initialize_I2C();
+
+//   unsigned int light_data = 0;
+
+//   // RN=0111b=7 The LSB bit is worth 1.28
+//   // CT=0 Result produced in 100 ms
+//   // M=11b=3 Continuous readings
+//   // ME=1 Mask (hide) the Exponent from the result
+//   //  RN  CT M   ...        ME
+//   //   |  |  |              |
+//   //   v  v  v              v
+//   // 0111 0  11 0 0 0 0 1 0 1 00 = 0x7614
+//   i2c_write_word(0x44, 0x01, 0x7614);
+
+//   for (;;) {
+//     i2c_read_word(0x44, 0x00, &light_data);
+//     uart_write_uint16(light_data * 1.28); // multiply by 1.28 for the lux
+//     value
+//     __delay_cycles(1000000);
+//   }
+
+//   return 0;
+// }
+
+// Part 7.3: Lux Logger
 int main() {
   WDTCTL = WDTPW | WDTHOLD; // stop watchdog timer
   PM5CTL0 &= ~LOCKLPM5;
@@ -57,7 +89,13 @@ int main() {
   Initialize_UART();
   Initialize_I2C();
 
-  unsigned int light_data = 0;
+  // ** Button Setup **
+  P1DIR &= ~(BUT2); // 0: Direct pin as input
+  P1REN |= (BUT2);  // 1: Enable built-in resistor
+  P1OUT |= (BUT2);  // 1: Set resistor as pull-up
+  P1IES |= (BUT2);  // 1: interrupt on falling edge (0 for rising edge)
+  P1IFG &= ~(BUT2); // 0: clear the interrupt flags
+  P1IE |= (BUT2);   // 1: enable the interrupts
 
   // RN=0111b=7 The LSB bit is worth 1.28
   // CT=0 Result produced in 100 ms
@@ -69,9 +107,29 @@ int main() {
   // 0111 0  11 0 0 0 0 1 0 1 00 = 0x7614
   i2c_write_word(0x44, 0x01, 0x7614);
 
+  unsigned int light_data = 0, lux = 0;
+  i2c_read_word(0x44, 0x00, &light_data);
+  lux = light_data * 1.28;
+  unsigned int light_max = lux + 10, light_min = lux - 10;
+
+  char *time = "12:00\t";
+  uart_write_string("*** Lux Logger ***\n\n");
+
   for (;;) {
-    i2c_read_word(0x44, 0x00, &light_data);
-    uart_write_uint16(light_data * 1.28);
+    i2c_read_word(0x44, 0x00, &light_data); // get reading
+    lux = light_data * 1.28;
+    uart_write_string(time);
+    uart_write_uint16(lux);
+
+    if (lux > light_max) {
+      uart_write_string("\t<Up>");
+      light_max = lux;
+    } else if (lux < light_min) {
+      uart_write_string("\t<Down>");
+      light_min = lux;
+    }
+    uart_write_char('\n');
+
     __delay_cycles(1000000);
   }
 
@@ -242,5 +300,14 @@ void uart_write_string(char *str) {
   for (i = 0; i < n; i++) { // loop through chars abnd print
     uart_write_char(str[i]);
   }
-  uart_write_char('\n');
+}
+
+// ** ISR for Part 3 **
+#pragma vector = PORT1_VECTOR // Link the ISR to the vector
+__interrupt void P1_ISR() {
+  // If S2 is pressed, set time
+  if ((P1IFG & BUT2) != 0) {
+
+    __delay_cycles(500000);
+  }
 }
